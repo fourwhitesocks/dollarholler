7b5f2dc956ece12297a320f0f648ab4496b2ada8
amy stout <asbilly92@gmail.com>
2022-09-30 09:01:05 -0400

    refactor: :sparkles: completed thru lesson 12 current path one

---
+++ src/routes/(dashboard)/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Navbar from '$lib/components/navbar.svelte';
+</script>
+
+<div class="grid min-h-screen grid-cols-12 gap-x-16 bg-whisper">
+  <Navbar />
+  <main class="col-span-8 pt-10"><slot><!-- optional fallback --></slot></main>
+</div>
--- src/routes/invoices/[id]/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { error } from "@sveltejs/kit"
-
-export function load({ params }) {
-    if (params.id) return { id: params.id }
-
-    throw error(404, 'Not Found silly!')
-}
-
-//+page.ts file runs w/ the .svelte file which means it runs on the server during server side rendering, and in the browser during client side navigation!
