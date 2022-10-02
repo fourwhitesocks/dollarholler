0063190cca96773b6a9b4ca98914f6a361d23e6e
amy stout <asbilly92@gmail.com>
2022-10-02 19:45:09 -0400

    feat: Resp pass on dashboard chapter

--- src/routes/(dashboard)/invoices/+page.svelte
+++ src/routes/(dashboard)/invoices/+page.svelte
@@ -1,1 +1,28 @@
-invoices
+invoices Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit obcaecati porro
+veritatis repudiandae iste nesciunt accusamus doloremque nam esse omnis eius cumque facere ipsa
+similique corrupti, minima tenetur magnam ab voluptatum commodi delectus ipsum deleniti recusandae
+officiis! Animi totam itaque magnam! Nemo, quaerat. Nisi non optio delectus. Omnis quod vitae animi
+veritatis. Vel numquam ea, necessitatibus tempora quia repellat quo. Lorem ipsum dolor, sit amet
+consectetur adipisicing elit. Dolorum, expedita vero? Iure, consectetur dolorem tempora fuga
+repellat illum asperiores. Voluptatum, vitae obcaecati est atque tempore minus enim sapiente
+accusamus, repellendus aut illum assumenda ad necessitatibus deserunt, totam soluta. Temporibus vel
+et nihil repellat, dignissimos ipsa sit, nemo numquam harum corrupti similique perferendis quibusdam
+autem culpa dolore odio est inventore accusantium eos rerum, minus iusto repudiandae fuga? Officiis,
+ipsa tempora. Culpa eum distinctio accusantium alias minus quis, consequuntur ut in laboriosam quam
+corrupti esse velit ipsam hic incidunt quae adipisci, exercitationem voluptate ea nulla temporibus
+nam maiores expedita ratione. Unde numquam sint deleniti ut dolores voluptatem nostrum suscipit
+dignissimos error adipisci cum, ullam earum officia quos consequatur excepturi dolore totam rem
+explicabo omnis laboriosam voluptates obcaecati. Asperiores, molestiae enim quam voluptates pariatur
+voluptate vel labore distinctio porro quia quis sed esse fugit dicta nesciunt amet reiciendis
+facilis perspiciatis at officia quisquam alias voluptas nam obcaecati. Quisquam culpa illo ea ipsa
+rem minus repellat odit dolorem porro quod, repudiandae veniam. Nulla, impedit saepe non esse
+pariatur earum qui suscipit neque. Officia consectetur optio est odit, minus minima corrupti, quam
+dolore, excepturi quidem impedit? Itaque perferendis fugit, culpa eius in dolor accusantium est
+maiores possimus. Aperiam error id ea exercitationem nam est vero veniam corrupti mollitia a
+officiis velit inventore voluptas pariatur provident atque adipisci praesentium consequatur aliquam,
+minus tempora cupiditate. Possimus tempora odit asperiores consequatur, odio natus facere velit
+animi iusto vel eaque aut mollitia nemo quas aperiam quis excepturi laborum ex. Eveniet explicabo,
+quas a dolorum nihil enim. Eveniet ex ipsa in, fuga corporis nesciunt ad, ea voluptates odit eum
+maiores corrupti soluta voluptate exercitationem magni error? Perferendis facilis necessitatibus
+dicta eum dolor accusamus sint, quis, voluptate dolorum magni, incidunt corrupti debitis id quod!
+Ex, unde cum ullam placeat fuga aliquam!
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -26,6 +26,11 @@ extend: {
     caribbeanGreen: '#00ca9e',
     fog: '#decaec',
   },
+  zIndex: {
+    navBarToggle: 91,
+    navBar: 90,
+
+  }
     }
   },
   plugins: []
